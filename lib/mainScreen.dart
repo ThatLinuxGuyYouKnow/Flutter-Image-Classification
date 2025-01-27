207b7b5f6f789968838d057cacf8d2174e60fba0
Emmanuels-code <arocket04@gmail.com>
2025-01-27 11:37:38 +0100

    chore: created new main screen

---
+++ lib/mainScreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  MainScreen({super.key});
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(),
+    );
+  }
+}
